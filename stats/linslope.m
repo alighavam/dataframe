d6dc3f84875cb9d0c6f1671687422012a7ecbb3b
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2023-12-18 10:59:04 -0500

    Update linslope.m

--- stats/linslope.m
+++ stats/linslope.m
@@ -1,6 +1,13 @@
-function b=linslope(X);
+function b=linslope(X, varargin)
 % function b=linslope([x y]);
 % slope from linear regression with intercept 
 % utility function for pivottables 
-b=linregress(X(:,2),X(:,1),'intercept',1);
-b=b(2);
+
+intercept = 1;
+vararginoptions(varargin,{'intercept'})
+
+b=linregress(X(:,2),X(:,1),'intercept',intercept);
+
+if (intercept)
+    b=b(2);
+end
